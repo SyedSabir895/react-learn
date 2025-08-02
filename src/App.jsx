8b1d3e270b484cd63cdd0a1542e668cb47ce48f2
SyedSabir895 <sabirsyed2005@gmail.com>
2025-08-02 09:27:34 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import UserList from './UserList'
 import Weather from './Weather'
 
 function App() {
